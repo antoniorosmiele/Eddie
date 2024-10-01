1454cacefdbd19e4ba577a1df7cc4f13a9e42e5f
cysko7927 <cysko7927@gmail.com>
2024-10-01 20:29:31 +0200

    Added an example for testing

---
+++ linux/examples/TestHardwareLed.cpp
@@ -0,0 +1,22 @@
+#include <thread>
+#include <csignal>
+#include <fstream>
+#include "../hardware/include/GPIOWriter.h"
+#include <iostream>
+
+int main(int argc, char *argv[]) 
+{
+    GPIOWriter writer = GPIOWriter(27, true);     
+
+    while (true)
+    {
+        writer.write(1);
+        std:: cout << "on" << "\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+        writer.write(0);
+        std:: cout << "off" << "\n";
+    }
+                               
+
+    
+}
