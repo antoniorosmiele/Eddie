5b2992568b5651dfe3a0a68b326317e57fb2a540
cysko7927 <cysko7927@gmail.com>
2024-10-06 20:25:34 +0200

    Added reader temperature

--- linux/examples/TestHardwareLed.cpp
+++ linux/examples/TestHardwareLed.cpp
@@ -2,11 +2,13 @@
 #include <csignal>
 #include <fstream>
 #include "../hardware/include/GPIOWriter.h"
+#include "../hardware/include/OneWireReader.h"
 #include <iostream>
 
 int main(int argc, char *argv[]) 
 {
     GPIOWriter writer = GPIOWriter(27, true);     
+    OneWireReader reader = OneWireReader("28-3c01f09582ff", true);
 
     while (true)
     {
@@ -16,6 +18,10 @@ int main(int argc, char *argv[])
         writer.write(0);
         std:: cout << "off" << "\n";
         std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+
+        long temperature = reader.read_value();
+        std:: cout << "temperature: "  << temperature << "\n";
+        
     }
                                
 
